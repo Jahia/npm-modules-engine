e226c03e91524e99f0e813beeaea53edc4220acf
Thomas Draier <tdraier@jahia.com>
2021-04-22 11:55:00 +0200

    Cypress test fixes

--- tests/cypress/plugins/env.js
+++ tests/cypress/plugins/env.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 
 module.exports = (on, config) => {
-    dotenv.config()
+    dotenv.config({path:'./run-artifacts/cypress.env'})
 
     config.baseUrl = process.env.JAHIA_URL;
     config.env.SUPER_USER_PASSWORD = process.env.SUPER_USER_PASSWORD;
