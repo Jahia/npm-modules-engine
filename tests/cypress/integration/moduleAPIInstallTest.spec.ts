ede8a0a7ffc9e1e5e938f8266c41c73e3d69b322
Rafael Fonseca <rfonseca85@yahoo.ca>
2021-04-29 16:55:19 -0400

    Removing a test to check on cypress (#7)
    * Removing a test to check on cypress
    
    * Reactivating test in diff position to make sure theres no npm bundle installed when trying this test
    
    * Removing unnecessary test
    
    * Tentative to fix the test
    
    * Tentative to fix the test
    
    * Adding the test back
    
    * Separating tests in diff files so remove conflict
    
    * Fixing lint pretty pretty
    
    * Removing docker copy again to check if conflicts still happening

--- tests/cypress/integration/moduleAPIInstallTest.spec.ts
+++ tests/cypress/integration/moduleAPIInstallTest.spec.ts
@@ -21,42 +21,4 @@ describe('Install NPM Modules via Module Manager API', () => {
                 })
             })
     })
-
-    it('Should be Unauthorized', () => {
-        // Perform the request
-        cy.apiRequest('POST', bundleApiUrl, null, null, null, (response) => {
-            expect(response.status).to.eq(401)
-            expect(response.response).to.contains('User guest is not allowed')
-        })
-    })
-
-    it('Should fail requesting at least one bundle', () => {
-        // Build up the form
-        const formData = new FormData()
-        formData.set('start', 'true') //adding a plain input to the form
-
-        cy.apiRequest('POST', bundleApiUrl, formData, authorization, null, (response) => {
-            expect(response.status).to.eq(400) //Bad Request
-            expect(response.response).to.contains('At least one bundle file is required')
-        })
-    })
-
-    it('Should fail installing corrupted bundle', () => {
-        const fileName = 'dummy-corrupted.tgz'
-
-        cy.fixture(fileName, 'binary')
-            .then((binary) => Cypress.Blob.binaryStringToBlob(binary))
-            .then((blob) => {
-                // Build up the form
-                const formData = new FormData()
-                formData.set('bundle', blob, fileName) //adding a file to the form
-                formData.set('start', 'true') //adding a plain input to the form
-
-                cy.apiRequest('POST', bundleApiUrl, formData, authorization, null, (response) => {
-                    expect(response.status).to.eq(500)
-                    expect(response.response).to.contains('Error while installing bundle')
-                    expect(response.response).to.contains('java.util.zip.ZipException: Not in GZIP format')
-                })
-            })
-    })
 })
