f66bbfe1fd997c01808c214e69798c9f0d4950f9
Francois G <fgerthoffert@jahia.com>
2023-05-15 14:14:35 +0200

    Some more renoming

--- tests/cypress/integration/ui/manageModulesSucessTgzTest.spec.ts
+++ tests/cypress/integration/ui/manageModulesSucessTgzTest.spec.ts
@@ -4,6 +4,6 @@ describe('Install TGZ bundle success path', () => {
     it('Install tgz bundle via UI success path', function () {
         manageModules.goTo()
         manageModules.uploadModule('npm-module-example-v1.0.0.tgz')
-        manageModules.assertModuleInResults('npm-plugin-example')
+        manageModules.assertModuleInResults('npm-module-example')
     })
 })
