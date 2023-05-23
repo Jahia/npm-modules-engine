3d6029201a844b5206a57ae33a91bd7f9e17ae90
Serge Huber <shuber@jahia.com>
2023-05-23 09:47:59 +0200

    ACADEMY-968 Cleanup var args for arr helper

--- src/javascript/handlebars/helpers/util/arr.js
+++ src/javascript/handlebars/helpers/util/arr.js
@@ -1,10 +1,10 @@
-export default function () {
+export default function (...arrayEntries) {
     // Last argument is the options object.
     const result = [];
 
     // Skip the last argument.
-    for (var i = 0; i < arguments.length - 1; ++i) {
-        result.push(arguments[i]);
+    for (var i = 0; i < arrayEntries.length - 1; ++i) {
+        result.push(arrayEntries[i]);
     }
 
     return result;
