a1873c828274f947a4fadcca4da7139b365aeaee
Kevan <kevan@jahia.com>
2023-09-05 16:04:15 +0200

    BACKLOG-21008: lint

--- tests/cypress/e2e/ui/getNode.cy.ts
+++ tests/cypress/e2e/ui/getNode.cy.ts
@@ -4,10 +4,19 @@ describe('Test on getNode helper', () => {
         cy.visit(`/cms/render/default/en/sites/npmTestSite/home/testPage.html`)
         cy.get('div[data-testid="currentNode_properties_prop1"]').should('contain', 'prop1 value')
         cy.get('div[data-testid="currentNode_properties_jcr:title"]').should('contain', 'NPM test component')
-        cy.get('div[data-testid="currentNode_properties_propMultiple"]').should('contain', '[value 1, value 2, value 3]')
+        cy.get('div[data-testid="currentNode_properties_propMultiple"]').should(
+            'contain',
+            '[value 1, value 2, value 3]',
+        )
         cy.get('div[data-testid="currentNode_name"]').should('contain', 'test')
-        cy.get('div[data-testid="currentNode_path"]').should('contain', '/sites/npmTestSite/home/testPage/pagecontent/test')
-        cy.get('div[data-testid="currentNode_parent"]').should('contain', '/sites/npmTestSite/home/testPage/pagecontent')
+        cy.get('div[data-testid="currentNode_path"]').should(
+            'contain',
+            '/sites/npmTestSite/home/testPage/pagecontent/test',
+        )
+        cy.get('div[data-testid="currentNode_parent"]').should(
+            'contain',
+            '/sites/npmTestSite/home/testPage/pagecontent',
+        )
         cy.get('div[data-testid="currentNode_nodeType"]').should('contain', 'npmExample:test')
         cy.logout()
     })
@@ -17,7 +26,10 @@ describe('Test on getNode helper', () => {
         cy.visit(`/cms/render/default/en/sites/npmTestSite/home/testPage.html`)
 
         cy.get('div[data-testid="getNode_site_withChildren_contents_name"]').should('contain', 'contents')
-        cy.get('div[data-testid="getNode_site_withChildren_contents_path"]').should('contain', '/sites/npmTestSite/contents')
+        cy.get('div[data-testid="getNode_site_withChildren_contents_path"]').should(
+            'contain',
+            '/sites/npmTestSite/contents',
+        )
         cy.get('div[data-testid="getNode_site_withChildren_contents_nodeType"]').should('contain', 'jnt:contentFolder')
 
         cy.get('div[data-testid="getNode_site_withChildren_home_name"]').should('contain', 'home')
