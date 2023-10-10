f6dfd20c2353134b2a9d17429690376966a55776
Kevan <kevan@jahia.com>
2023-10-10 11:59:20 +0200

    BACKLOG-21543: inject ctx in handlebars views

--- src/javascript/handlebars/helpers/render/index.js
+++ src/javascript/handlebars/helpers/render/index.js
@@ -1,4 +1,3 @@
-export {default as renderInfo} from './renderInfo';
 export {default as addResources} from './addResources';
 export {default as addCacheDependency} from './addCacheDependency';
 export {default as render} from './render';
