e236b912b8cc8f2d6c28a9a1111a688d4312109c
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2023-10-25 09:40:13 +0200

    lint --fix

--- tests/cypress/e2e/ui/simplePageLink.cy.ts
+++ tests/cypress/e2e/ui/simplePageLink.cy.ts
@@ -1,4 +1,4 @@
-import { addNode, createSite, deleteSite, enableModule, publishAndWaitJobEnding } from "@jahia/cypress";
+import { addNode, createSite, deleteSite, enableModule, publishAndWaitJobEnding } from '@jahia/cypress'
 
 const siteKey = 'simplePageSite'
 const pageName = 'simple'
@@ -10,45 +10,49 @@ const variables = {
     primaryNodeType: 'jnt:page',
     template: 'home',
     properties: [
-        {name: 'jcr:title', value: pageTitle, language: 'en'},
-        {name: 'j:templateName', type: 'STRING', value: 'events'}
+        { name: 'jcr:title', value: pageTitle, language: 'en' },
+        { name: 'j:templateName', type: 'STRING', value: 'events' },
     ],
-    children: [{
-        name: 'area-main',
-        primaryNodeType: 'jnt:contentList',
-        children: [{
-            name: 'text',
-            primaryNodeType: 'jnt:text',
-            properties: [{language: 'en', name: 'text', type: 'STRING', value: pageName}]
-        }]
-    }]
-};
+    children: [
+        {
+            name: 'area-main',
+            primaryNodeType: 'jnt:contentList',
+            children: [
+                {
+                    name: 'text',
+                    primaryNodeType: 'jnt:text',
+                    properties: [{ language: 'en', name: 'text', type: 'STRING', value: pageName }],
+                },
+            ],
+        },
+    ],
+}
 
 describe('Page should create a link to preview and live', () => {
     before('createSite and page', () => {
-        cy.login();
-        createSite(siteKey);
-        enableModule('calendar', siteKey);
-        addNode(variables);
-        publishAndWaitJobEnding(`/sites/${siteKey}/home`, ['en']);
-        cy.logout();
+        cy.login()
+        createSite(siteKey)
+        enableModule('calendar', siteKey)
+        addNode(variables)
+        publishAndWaitJobEnding(`/sites/${siteKey}/home`, ['en'])
+        cy.logout()
     })
 
     it('test preview', () => {
-        cy.login();
-        cy.visit(`/cms/render/default/en/sites/${siteKey}/home/${pageName}.html`);
-        cy.get(':contains("Page not found")').should('not.exist');
-        cy.logout();
+        cy.login()
+        cy.visit(`/cms/render/default/en/sites/${siteKey}/home/${pageName}.html`)
+        cy.get(':contains("Page not found")').should('not.exist')
+        cy.logout()
     })
 
     it('test live', () => {
-        cy.login();
-        cy.visit(`/cms/render/live/en/sites/${siteKey}/home/${pageName}.html`);
-        cy.get(':contains("Page not found")').should('not.exist');
-        cy.logout();
+        cy.login()
+        cy.visit(`/cms/render/live/en/sites/${siteKey}/home/${pageName}.html`)
+        cy.get(':contains("Page not found")').should('not.exist')
+        cy.logout()
     })
 
     after('delete site', () => {
-        deleteSite(siteKey);
+        deleteSite(siteKey)
     })
 })
