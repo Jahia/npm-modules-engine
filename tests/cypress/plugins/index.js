ac9e2ffb31d590325e388c2825b783fd16b34150
Serge Huber <shuber@jahia.com>
2023-10-27 12:12:45 +0200

    QA-14997 Fix nightly test flakyness (#84)
    * QA-14997 Fix nightly test flakyness
    - Test by deactivating code coverage plugins (is it actually used ?)
    
    * QA-14997 Fix nightly test flakyness
    - Added comment to explain why it is deactivated

--- tests/cypress/plugins/index.js
+++ tests/cypress/plugins/index.js
@@ -1,20 +1,22 @@
+// QA-14997 temporarily deactivated since it causing flakyness in tests
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const codeCoverage = require('@cypress/code-coverage/task');
+// const codeCoverage = require('@cypress/code-coverage/task')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const installLogsPrinter = require('cypress-terminal-report/src/installLogsPrinter');
-const env = require('./env');
+const installLogsPrinter = require('cypress-terminal-report/src/installLogsPrinter')
+const env = require('./env')
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
+const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor')
 
 module.exports = (on, config) => {
     env(on, config)
-    codeCoverage(on, config);
-    
+    // QA-14997 temporarily deactivated since it causing flakyness in tests
+    // codeCoverage(on, config);
+
     //https://github.com/archfz/cypress-terminal-report
-    installLogsPrinter(on);
-    on("file:preprocessor", cypressTypeScriptPreprocessor);
+    installLogsPrinter(on)
+    on('file:preprocessor', cypressTypeScriptPreprocessor)
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     require('@jahia/cypress/dist/plugins/registerPlugins').registerPlugins(on, config)
-    return config;
-};
+    return config
+}
