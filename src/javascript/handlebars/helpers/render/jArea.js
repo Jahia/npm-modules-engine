29fa9339beef5188fa46021d053bda394d4800d7
Janin-Michel-Mathias <jmichelmathias@jahia.com>
2023-11-07 14:48:30 +0100

    lint --fix

--- src/javascript/handlebars/helpers/render/jArea.js
+++ src/javascript/handlebars/helpers/render/jArea.js
@@ -1,18 +1,21 @@
 import {render} from '@jahia/server-helpers';
 import {SafeString} from 'handlebars';
-import {setResult} from "../../util";
+import {setResult} from '../../util';
 
 const allowedAreaProperties = ['areaView', 'allowedTypes', 'numberOfItems', 'subNodesView'];
 
-export default function(options) {
+export default function (options) {
     const attrs = {};
-     attrs.content = Object.keys(options.hash).reduce((acc, attr) => {
-        if(allowedAreaProperties.includes(attr)) {
-            if(!acc.properties) acc.properties = {};
-            acc.properties['j:'+attr] = options.hash[attr];
-        } else if(attr === 'name') {
-            acc.name = options.hash.name
-        }else {
+    attrs.content = Object.keys(options.hash).reduce((acc, attr) => {
+        if (allowedAreaProperties.includes(attr)) {
+            if (!acc.properties) {
+                acc.properties = {};
+            }
+
+            acc.properties['j:' + attr] = options.hash[attr];
+        } else if (attr === 'name') {
+            acc.name = options.hash.name;
+        } else {
             console.warn(`jArea helper: ${attr} is not a valid attribute, attribute ignored`);
         }
 
@@ -24,7 +27,7 @@ export default function(options) {
         return '';
     }
 
-    if(!attrs.content.name) {
+    if (!attrs.content.name) {
         console.warn('jArea helper: name is a mandatory property');
         return '';
     }
--- tests/cypress/e2e/ui/jAreaTest.cy.ts
+++ tests/cypress/e2e/ui/jAreaTest.cy.ts
@@ -1,45 +1,52 @@
-import { addNode, createSite, deleteSite, enableModule, getNodeAcl, getNodeByPath, waitUntilJournalSync } from "@jahia/cypress"
-import 'cypress-wait-until';
+import { addNode, createSite, deleteSite, enableModule } from '@jahia/cypress'
+import 'cypress-wait-until'
 
-const siteKey = 'jAreaHelperTestSite';
+const siteKey = 'jAreaHelperTestSite'
 
 describe('jArea helper test', () => {
     before('Create site and content', () => {
-        cy.login();
-        createSite(siteKey, { templateSet: 'npm-module-example', serverName: 'localhost', locale: 'en' });
-        enableModule('event', siteKey);
-        cy.visit(`/jahia/jcontent/${siteKey}/en/pages/home`);
+        cy.login()
+        createSite(siteKey, { templateSet: 'npm-module-example', serverName: 'localhost', locale: 'en' })
+        enableModule('event', siteKey)
+        cy.visit(`/jahia/jcontent/${siteKey}/en/pages/home`)
         cy.iframe('#page-builder-frame-1').within(() => {
-        cy.get('div[class="article"]').find('div[type="placeholder"]').should('be.visible').then(() => {
-            addNode({
-                parentPathOrId: `/sites/${siteKey}/home/pagecontent`,
-                name: 'testAreas',
-                primaryNodeType: 'npmExample:testAreas'
-            });
-        });
+            cy.get('div[class="article"]')
+                .find('div[type="placeholder"]')
+                .should('be.visible')
+                .then(() => {
+                    addNode({
+                        parentPathOrId: `/sites/${siteKey}/home/pagecontent`,
+                        name: 'testAreas',
+                        primaryNodeType: 'npmExample:testAreas',
+                    })
+                })
         })
-        cy.logout();
-    });
+        cy.logout()
+    })
 
     beforeEach('Login and visit', () => {
-        cy.login();
-        cy.visit(`/jahia/jcontent/${siteKey}/en/pages/home`);
+        cy.login()
+        cy.visit(`/jahia/jcontent/${siteKey}/en/pages/home`)
     })
 
     it('Basic Area test', () => {
         cy.iframe('#page-builder-frame-1').within(() => {
-            cy.get('div[data-testid="basicArea"]').find('div[type="area"]').should('be.visible');
+            cy.get('div[data-testid="basicArea"]').find('div[type="area"]').should('be.visible')
         })
     })
 
     it('Allowed types area', () => {
         cy.iframe('#page-builder-frame-1').within(() => {
-            cy.get('div[data-testid="allowedTypesArea"]').find('div[type="placeholder"]').then(buttons => {
-                const selector = `div[data-jahia-id="${buttons.attr('id')}"]`;
-                cy.get(selector).find('button[data-sel-role="jnt:bigText"]').should('be.visible');
-                cy.get(selector).find('button[data-sel-role="jnt:event"]').should('be.visible');
-                cy.get(selector).find('button[data-sel-role!="jnt:event"][data-sel-role!="jnt:bigText"]').should('not.exist');
-            })
+            cy.get('div[data-testid="allowedTypesArea"]')
+                .find('div[type="placeholder"]')
+                .then((buttons) => {
+                    const selector = `div[data-jahia-id="${buttons.attr('id')}"]`
+                    cy.get(selector).find('button[data-sel-role="jnt:bigText"]').should('be.visible')
+                    cy.get(selector).find('button[data-sel-role="jnt:event"]').should('be.visible')
+                    cy.get(selector)
+                        .find('button[data-sel-role!="jnt:event"][data-sel-role!="jnt:bigText"]')
+                        .should('not.exist')
+                })
         })
     })
 
@@ -47,22 +54,22 @@ describe('jArea helper test', () => {
         addNode({
             parentPathOrId: `/sites/${siteKey}/home/numberOfItemsArea`,
             name: 'item1',
-            primaryNodeType: 'jnt:bigText'
-        });
+            primaryNodeType: 'jnt:bigText',
+        })
         addNode({
             parentPathOrId: `/sites/${siteKey}/home/numberOfItemsArea`,
             name: 'item2',
-            primaryNodeType: 'jnt:bigText'
-        });
-        cy.reload();
+            primaryNodeType: 'jnt:bigText',
+        })
+        cy.reload()
         cy.iframe('#page-builder-frame-1').within(() => {
-            cy.get('div[data-testid="numberOfItemsArea"]').find('div[type="placeholder"]').should('not.be.visible');
+            cy.get('div[data-testid="numberOfItemsArea"]').find('div[type="placeholder"]').should('not.be.visible')
         })
     })
 
     it('areaView Area', () => {
         cy.iframe('#page-builder-frame-1').within(() => {
-            cy.get('div[data-testid="areaViewArea"]').find('ul[class*="dropdown"]').should('be.visible');
+            cy.get('div[data-testid="areaViewArea"]').find('ul[class*="dropdown"]').should('be.visible')
         })
     })
 
@@ -70,22 +77,22 @@ describe('jArea helper test', () => {
         addNode({
             parentPathOrId: `/sites/${siteKey}/home/subNodesViewArea`,
             name: 'item1',
-            primaryNodeType: 'jnt:bigText'
-        });
-        cy.reload();
+            primaryNodeType: 'jnt:bigText',
+        })
+        cy.reload()
         cy.iframe('#page-builder-frame-1').within(() => {
-            cy.get('div[data-testid="subNodesViewArea"]').find('a').contains('item1');
+            cy.get('div[data-testid="subNodesViewArea"]').find('a').contains('item1')
         })
     })
 
     afterEach('Logout', () => {
         cy.logout()
-        cy.visit('/');
-    });
+        cy.visit('/')
+    })
 
     after('Delete site', () => {
-        cy.login();
-        deleteSite(siteKey);
-        cy.logout();
+        cy.login()
+        deleteSite(siteKey)
+        cy.logout()
     })
 })
