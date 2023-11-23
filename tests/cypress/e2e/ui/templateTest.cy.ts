6c3e786cfcc8f7ae54c6441ce2a28efa04bd380b
Kevan <kevan@jahia.com>
2023-11-23 18:27:08 +0100

    BACKLOG-21746: more cypress test for React coverage

--- tests/cypress/e2e/ui/templateTest.cy.ts
+++ tests/cypress/e2e/ui/templateTest.cy.ts
@@ -1,7 +1,6 @@
-import { createSite, deleteSite, publishAndWaitJobEnding } from '@jahia/cypress'
+import { publishAndWaitJobEnding } from '@jahia/cypress'
 import 'cypress-iframe'
-
-const siteKey = 'templateTestSite'
+import { addSimplePage } from '../../utils/Utils'
 
 const checkSectionsPresence = () => {
     cy.get('div[class="header"]').should('be.visible')
@@ -12,40 +11,49 @@ const checkSectionsPresence = () => {
 
 describe('Template testsuite', () => {
     before('Create site with template', () => {
-        createSite(siteKey, { templateSet: 'npm-module-example', serverName: 'localhost', locale: 'en' })
-    })
+        addSimplePage(`/sites/npmTestSite/home`, 'testTemplate', 'testTemplate', 'en', 'simple', [
+            {
+                name: 'pagecontent',
+                primaryNodeType: 'jnt:contentList',
+            },
+        ])
 
-    beforeEach('login & visit', () => {
-        cy.login()
-        cy.visit(`/jahia/jcontent/${siteKey}/en/pages/home`)
+        addSimplePage(`/sites/npmTestSite/home`, 'testTemplateReact', 'testTemplateReact', 'en', 'simpleReact', [
+            {
+                name: 'pagecontent',
+                primaryNodeType: 'jnt:contentList',
+            },
+        ])
     })
+    ;['testTemplate', 'testTemplateReact'].forEach((pageName) => {
+        it(`${pageName}: Verify 4 sections presence`, () => {
+            cy.login()
+            cy.visit(`/jahia/jcontent/npmTestSite/en/pages/home/${pageName}`)
+            cy.iframe('#page-builder-frame-1').within(() => {
+                checkSectionsPresence()
+            })
+            cy.logout()
+        })
 
-    afterEach('logout', () => {
-        cy.logout()
-    })
+        it(`${pageName}: Check grouping components`, () => {
+            cy.login()
+            cy.visit(`/jahia/jcontent/npmTestSite/en/pages/home/${pageName}`)
+            cy.iframe('#page-builder-frame-1').within(() => {
+                cy.get('button[data-sel-role="createContent"]:first').click()
+            })
+            cy.get('li[role="treeitem"]:contains("npmExampleComponent")').click()
+            cy.get('li[role="treeitem"]:contains("Navigation Menu")').should('be.visible')
+            cy.get('li[role="treeitem"]:contains("test")').should('be.visible')
+            cy.logout()
+        })
 
-    after('Delete site', () => {
-        deleteSite(siteKey)
-    })
-
-    it('Verify 4 sections presence', () => {
-        cy.iframe('#page-builder-frame-1').within(() => {
+        it(`${pageName}: Check 4 sections presence in LIVE workspace`, () => {
+            cy.login()
+            cy.visit(`/jahia/jcontent/npmTestSite/en/pages/home/${pageName}`)
+            publishAndWaitJobEnding('/sites/npmTestSite')
+            cy.visit(`/sites/npmTestSite/home/${pageName}.html`)
             checkSectionsPresence()
+            cy.logout()
         })
-    })
-
-    it('Check grouping components', () => {
-        cy.iframe('#page-builder-frame-1').within(() => {
-            cy.get('button[data-sel-role="createContent"]:first').click()
-        })
-        cy.get('li[role="treeitem"]:contains("npmExampleComponent")').click()
-        cy.get('li[role="treeitem"]:contains("Navigation Menu")').should('be.visible')
-        cy.get('li[role="treeitem"]:contains("test")').should('be.visible')
-    })
-
-    it('Check 4 sections presence in LIVE workspace', () => {
-        publishAndWaitJobEnding('/sites/' + siteKey)
-        cy.visit(`/sites/${siteKey}/home.html`)
-        checkSectionsPresence()
     })
 })
