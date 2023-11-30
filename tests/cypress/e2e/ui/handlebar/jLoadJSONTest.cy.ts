fa74694de0dc305f32c5e26556f3612d36da84d4
Kevan <kevan@jahia.com>
2023-11-30 14:29:57 +0100

    Fix cypress test project

--- tests/cypress/e2e/ui/handlebar/jLoadJSONTest.cy.ts
+++ tests/cypress/e2e/ui/handlebar/jLoadJSONTest.cy.ts
@@ -1,33 +1,33 @@
-import { addSimplePage } from '../../../utils/Utils'
-import { addNode } from '@jahia/cypress'
+import {addSimplePage} from '../../../utils/Utils';
+import {addNode} from '@jahia/cypress';
 
 describe('Test on loadJSON helper', () => {
     before('Create test page and contents', () => {
-        addSimplePage(`/sites/npmTestSite/home`, 'testJLoadJSON', 'testJLoadJSON', 'en', 'simple', [
+        addSimplePage('/sites/npmTestSite/home', 'testJLoadJSON', 'testJLoadJSON', 'en', 'simple', [
             {
                 name: 'pagecontent',
-                primaryNodeType: 'jnt:contentList',
-            },
+                primaryNodeType: 'jnt:contentList'
+            }
         ]).then(() => {
             addNode({
-                parentPathOrId: `/sites/npmTestSite/home/testJLoadJSON/pagecontent`,
+                parentPathOrId: '/sites/npmTestSite/home/testJLoadJSON/pagecontent',
                 name: 'test',
-                primaryNodeType: 'npmExample:testJLoadJSON',
-            })
-        })
-    })
+                primaryNodeType: 'npmExample:testJLoadJSON'
+            });
+        });
+    });
 
     it('Check loadJSON for view relative file', function () {
-        cy.login()
-        cy.visit(`/cms/render/default/en/sites/npmTestSite/home/testJLoadJSON.html`)
-        cy.get('div[data-testid="loadJSON-viewRelative"]').should('contain', 'This is a view relative component')
-        cy.logout()
-    })
+        cy.login();
+        cy.visit('/cms/render/default/en/sites/npmTestSite/home/testJLoadJSON.html');
+        cy.get('div[data-testid="loadJSON-viewRelative"]').should('contain', 'This is a view relative component');
+        cy.logout();
+    });
 
     it('Check loadJSON for project absolute file', function () {
-        cy.login()
-        cy.visit(`/cms/render/default/en/sites/npmTestSite/home/testJLoadJSON.html`)
-        cy.get('div[data-testid="loadJSON-absolute"]').should('contain', 'This is a global component')
-        cy.logout()
-    })
-})
+        cy.login();
+        cy.visit('/cms/render/default/en/sites/npmTestSite/home/testJLoadJSON.html');
+        cy.get('div[data-testid="loadJSON-absolute"]').should('contain', 'This is a global component');
+        cy.logout();
+    });
+});
