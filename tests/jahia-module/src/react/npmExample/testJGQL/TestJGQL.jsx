9c9f163391248222b093fad2e4df01a46ac832f6
Jérôme Blanchard <jblanchard@jahia.com>
2024-03-08 12:16:41 +0100

    BACKLOG-20935: test GraphQL Query

---
+++ tests/jahia-module/src/react/npmExample/testJGQL/TestJGQL.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {useQuery, useServerContext} from "@jahia/js-server-engine";
+
+export const TestJGQL = () => {
+    const {currentNode} = useServerContext();
+    const result = useQuery({
+        query: "query ($path:String!) { jcr { nodeByPath(path:$path) { name, properties { name, value } } } }",
+        variables: {path: currentNode.getPath()}
+    });
+    return (
+        <>
+            <div data-testid="npm-view">NPM (react) view working</div>
+            <hr/>
+
+            <h3>GraphQL</h3>
+            <div data-testid="gql-info-test" style={{padding: '10px', margin: '10px', border: '1px solid'}}>
+                <ul>
+                    {result.data.jcr.nodeByPath.properties.map((property) => (
+                        <li data-testid={property.name}>{property.name}={property.value}</li>
+                    ))}
+                </ul>
+            </div>
+            <hr/>
+        </>
+    )
+}
+
+TestJGQL.jahiaComponent = {
+    nodeType: 'npmExample:testJGQL',
+    name: 'react',
+    displayName: 'test JGQL (react)',
+    componentType: 'view'
+}
