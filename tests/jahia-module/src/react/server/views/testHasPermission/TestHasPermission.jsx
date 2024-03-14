1ed4b6c76197825decad27eb85f4ae8d73796125
Janin Michel-Mathias <107043504+Janin-Michel-Mathias@users.noreply.github.com>
2024-03-14 10:24:52 +0100

    BACKLOG-22402 Add tests in npm modules engine (#140)
    * Add tests in npm modules engine
    
    * Implement required changes

---
+++ tests/jahia-module/src/react/server/views/testHasPermission/TestHasPermission.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useServerContext } from '@jahia/js-server-engine';
+
+export const testHasPermission = () => {
+    const {currentNode} = useServerContext();
+    return (
+        <>
+            <h3>Node has permission</h3>
+            <div data-testid="currentNode_hasPermission">{currentNode.hasPermission('component-npmExampleMix_npmExampleComponent') ? 'true' : 'false'}</div>
+            <h3>Node doesn't have permission</h3>
+            <div data-testid="currentNode_hasNotPermission">{currentNode.hasPermission('fakePermission') ? 'true' : 'false'}</div>
+        </>
+    );
+}
+
+testHasPermission.jahiaComponent = {
+    nodeType: 'npmExample:testHasPermission',
+    name: 'default',
+    displayName: 'test has permission',
+    componentType: 'view'
+}
---
+++ tests/jahia-module/src/react/server/views/testIsNodeType/TestIsNodeType.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useServerContext } from '@jahia/js-server-engine';
+
+export const TestIsNodeType = () => {
+    const {currentNode} = useServerContext();
+    return (
+        <>
+            <h3>Current node type is testIsNodeType</h3>
+            <div data-testid="currentNode_isNodeType">{currentNode.isNodeType('npmExample:testIsNodeType') ? 'true' : 'false'}</div>
+
+            <h3>Current node type is not jnt:page</h3>
+            <div data-testid="currentNode_isNotNodeType">{currentNode.isNodeType('jnt:page') ? 'true' : 'false'}</div>
+        </>
+    );
+}
+
+TestIsNodeType.jahiaComponent = {
+    nodeType: 'npmExample:testIsNodeType',
+    name: 'default',
+    displayName: 'test is node type',
+    componentType: 'view'
+}
