887ffb4fc09875d9a0981da30f0a93e6cec496c0
Kevan <kevan@jahia.com>
2024-03-21 17:33:33 +0100

    BACKLOG-22456: remove unused import

--- tests/jahia-module/src/client/SampleHydrateInBrowserReact.jsx
+++ tests/jahia-module/src/client/SampleHydrateInBrowserReact.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
-import {useTranslation} from "react-i18next";
 
 const SampleHydrateInBrowserReact = ({initialValue}) => {
-    const { t } = useTranslation();
     const [count, setCount] = useState(initialValue);
 
     const handleClick = () => {
