e3c04e9f84be4d71d52c7b13884f6eab4adc9218
Tim ROBERT <robert.tim.web@gmail.com>
2024-08-19 11:54:39 +0200

    Add a platform check to run a platform specific sleep command in watch mode

--- tests/jahia-module/webpack.config.js
+++ tests/jahia-module/webpack.config.js
@@ -136,6 +136,8 @@ module.exports = (env, mode) => {
     // Also an additional sleep is added to avoid watch triggering too much in a short time
     // (Feel free to adjust the sleep time according to your needs)
     if (mode.watch) {
+        const sleepTime = 5;
+
         configs.push({
             name: 'watch',
             mode: 'development',
@@ -165,7 +167,7 @@ module.exports = (env, mode) => {
                         scripts: [
                             'yarn jahia-pack',
                             'yarn jahia-deploy',
-                            'sleep 5' // Sleep for 5 seconds, can be adjusted
+                            process.platform === 'win32' ? 'timeout ' + sleepTime : 'sleep ' + sleepTime,
                         ],
                         blocking: true,
                         parallel: false
