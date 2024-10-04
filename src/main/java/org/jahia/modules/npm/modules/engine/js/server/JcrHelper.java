10a8cc2da0a0f53b6a7f0d0262b12abdde9beac1
Baptiste Grimaud <bgrimaud@jahia.com>
2024-10-04 09:52:53 +0200

    BACKLOG-23051: Add Javadoc

--- src/main/java/org/jahia/modules/npm/modules/engine/js/server/JcrHelper.java
+++ src/main/java/org/jahia/modules/npm/modules/engine/js/server/JcrHelper.java
@@ -6,10 +6,26 @@ import org.jahia.services.usermanager.JahiaUserManagerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Helper class to perform JCR operations.
+ */
 public class JcrHelper {
 
     private static final Logger logger = LoggerFactory.getLogger(JcrHelper.class);
 
+    /**
+     * Execute JCR operations on a JCR session authenticated using the guest user and the "live" workspace.
+     * This is intended for server-side use. Example:
+     * <pre>
+     *     import {server, useServerContext} from '@jahia/js-server-core';
+     *     ...
+     *     const {renderContext, currentResource} = useServerContext();
+     *     server.jcr.doExecuteAsGuest(session => performJcrOperations(session, renderContext, currentResource));
+     * </pre>
+     *
+     * @param callback the callback to execute using the JCR session
+     * @return the result of the callback
+     */
     public Object doExecuteAsGuest(JCRCallback<Object> callback) {
         JCRTemplate template = JCRTemplate.getInstance();
         Object result = null;
